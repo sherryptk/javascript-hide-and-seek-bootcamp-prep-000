985bbd0428a4fa39094cf6ebffd9f8c12e4c3c48
Sherry <sherryptk@gmail.com>
2017-09-27 17:48:17 +0000

    Done.

--- index.js
+++ index.js
@@ -19,82 +19,10 @@ function increaseRankBy(n){
 
 
 function deepestChild(){
-
-    let current = document.getElementById('grand-node').querySelectorAll('div')
-    let next = []
+  let current = document.getElementById('grand-node').querySelectorAll('div')
+  let next = []
     for (let i = 0; i < current.length; i++) {
             next.push(current[i])
       }
-
       return next[next.length-1]
-
 }
-
-  // if (Array.isArray(current)) {
-  //      for (let i = 0; i < current.length; i++) {
-  //        next.push(current[i])
-  //        console.log(current[i]);
-  //      }
-  //    }
-
-     //current = next.shift()
-
-
-
-
-
-// function find(array, criteriaFn) {
-//   // initialize two variables, `current`, and `next`
-//   // `current` keeps track of the element that we're
-//   // currently on, just like we did when unpacking the
-//   // array above; `next` is itself an array that keeps
-//   // track of the elements (which might be arrays!) that
-//   // we haven't looked at yet
-//   let current = array
-//   let next = []
-//
-//   // hey, a `while` loop! this loop will only
-//   // trigger if `current` is truthy — so when
-//   // we exhaust `next` and, below, attempt to
-//   // `shift()` `undefined` (when `next` is empty)
-//   // onto `current`, we'll exit the loop
-//   while (current) {
-//     // if `current` satisfies the `criteriaFn`, then
-//     // return it — recall that `return` will exit the
-//     // entire function!
-//     if (criteriaFn(current)) {
-//       return current
-//     }
-//
-//     // if `current` is an array, we want to push all of
-//     // its elements (which might be arrays) onto `next`
-//     if (Array.isArray(current)) {
-//       for (let i = 0; i < current.length; i++) {
-//         next.push(current[i])
-//       }
-//     }
-//
-//     // after pushing any children (if there
-//     // are any) of `current` onto `next`, we want to take
-//     // the first element of `next` and make it the
-//     // new `current` for the next pass of the `while`
-//     // loop
-//     current = next.shift()
-//   }
-//
-//   // if we haven't
-//   return null
-// }
-
-//***************************************************
-  // Define a function deepestChild() that pulls out the most deeply nested child from div#grand-node.
-  // (Remember, you can iterate over elements and call querySelector() and querySelectorAll() on them.
-  // This is challenging to implement correctly, but not beyond your ability!)
-
-//   HINT 1: Your solution for deepestChild() does not need to be totally
-// generic; we don't expect it to work in every case. For example, we know that
-// div#grand-node has only one node at each level — for this lab, you can solve
-// for that case, and not worry about a case where there are sibling nodes.
-//
-// HINT: Remember learning about breadth-first search? A similar technique
-// might come in handy here.
